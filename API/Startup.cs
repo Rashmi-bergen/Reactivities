1976981fd8c1d52391abe679e841f1ec40e0a2e0
Rashmi-bergen <mesanaangel@gmail.com>
2021-01-03 21:01:22 +0100

    Added UI skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -27,8 +27,16 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(opt=>{
-               opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -42,11 +50,11 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+         
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
